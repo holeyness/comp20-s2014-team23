574aa8b3dfcaa4cf4def3e01b4b93ef9b5282362
Stephen Panaro <stephen.panaro@gmail.com>
2014-04-30 14:23:14 -0400

    move user/db interactions into user model

--- index.js
+++ index.js
@@ -5,79 +5,18 @@ var express = require('express')
   , exphbs  = require('express3-handlebars')
   , LocalStrategy = require('passport-local').Strategy;
 
-var userCollection = 'userCollection';
+// App modules
+var user = require('./models/user');
 
-var mongoUri = process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://localhost/nomify';
-
-function findById(id, fn) {
-  console.log("searching by ID");
-  mongo.Db.connect(mongoUri, function (err, db) {
-    db.collection(userCollection, function(err, collection) {
-      collection.find({"userId":id}).toArray(function(err, results) {
-        if (err) return fn(null, null);
-
-        console.log("FOUND a valid user by user id user id:" + JSON.stringify(results[0]));
-        return fn(null, results[0]);
-      });
-    });
-  });
-
-}
-
-function findByUsername(username, fn) {
-  mongo.Db.connect(mongoUri, function (err, db) {
-    db.collection(userCollection, function(er, collection) {
-      collection.find({"username":username}).toArray(function(err, results) {
-        if (err) return fn(null,null);
-
-        console.log("FOUND a valid user by username: " + JSON.stringify(results[0]));
-        return fn(null, results[0]);
-      });
-    });
-  });
-}
-
-
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.
-passport.serializeUser(function(user, done) {
-  done(null, user.userId);
-});
-
-passport.deserializeUser(function(id, done) {
-  findById(id, function (err, user) {
-    done(err, user);
-  });
-});
-
-
-// Use the LocalStrategy within Passport.
-//   Strategies in passport require a `verify` function, which accept
-//   credentials (in this case, a username and password), and invoke a callback
-//   with a user object.
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-
-      // Find the user by username.  If there is no user with the given
-      // username, or the password is not correct, set the user to `false` to
-      // indicate failure and set a flash message.  Otherwise, return the
-      // authenticated `user`.
-      findByUsername(username, function(err, user) {
-        if (err) { return done(err); }
-        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
-        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
-        return done(null, user);
-      })
-    });
+// Setup database.
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/nomify';
+var db;
+mongo.Db.connect(mongoUri, function (err, database) {
+  if (err) {
+    console.error("Failed to connect to Mongo with error: " + err);
   }
-));
+  db = database;
+});
 
 var app = express();
 
@@ -96,7 +35,7 @@ app.configure(function() {
   // Initialize Passport!  Also use passport.session() middleware, to support
   // persistent login sessions (recommended).
   app.use(passport.initialize());
-  app.use(passport.session());
+  app.use(passport.session()); // supports persistent login sessions
   app.use(flash());
 });
 
@@ -113,13 +52,7 @@ app.get('/login', function(req, res){
   res.render('login.html');
 });
 
-// POST /login
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
-//
-//   curl -v -d "username=bob&password=secret" http://127.0.0.1:3000/login
+//   test with: curl -v -d "username=bob&password=secret" http://127.0.0.1:3000/login
 app.post('/login',
   passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
   function(req, res) {
@@ -136,6 +69,47 @@ app.listen(3000, function() {
   console.log('Express server listening on port 3000');
 });
 
+
+//
+// Passport
+//
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.
+passport.serializeUser(function(user, done) {
+  done(null, user.Id);
+});
+
+passport.deserializeUser(function(id, done) {
+  user.findById(db, id, function (err, user) {
+    done(err, user);
+  });
+});
+
+// Use the LocalStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a username and password), and invoke a callback
+//   with a user object.
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+
+      // Find the user by username.  If there is no user with the given
+      // username, or the password is not correct, set the user to `false` to
+      // indicate failure.  Otherwise, return the authenticated `user`.
+      user.findByUsername(db, username, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+        return done(null, user);
+      })
+    });
+  }
+));
 
 // Simple route middleware to ensure user is authenticated.
 //   Use this route middleware on any resource that needs to be protected.  If
